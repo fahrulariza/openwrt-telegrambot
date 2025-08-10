09b17658ebfe1b553848e84accbcc1bd80687412
fahrulariza <131193627+fahrulariza@users.noreply.github.com>
2025-08-10 19:18:23 +0700

    Update reload_bot.py

--- cmd/reload_bot.py
+++ cmd/reload_bot.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import logging
 from telegram import Update
@@ -5,20 +6,25 @@ from telegram.ext import ContextTypes
 
 logger = logging.getLogger(__name__)
 
-async def execute(update: Update, context: ContextTypes.DEFAULT_TYPE, command_data: str = None) -> None:
-    """Menghentikan bot untuk memulai ulang."""
-    try:
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="⏳ Bot sedang di-restart. Mohon tunggu sebentar...lalu jalankan /start ulang",
-        )
-        
-        # Panggil skrip shell untuk melakukan restart
-        subprocess.Popen(["/www/assisten/bot/restart.sh"])
+async def execute(update: Update, context: ContextTypes.DEFAULT_TYPE, command_data: str) -> None:
+    chat_id = update.effective_chat.id
 
-    except Exception as e:
-        logger.error(f"Gagal menjalankan perintah restart: {e}")
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text=f"❌ Gagal me-restart bot. Terjadi kesalahan: `{e}`",
-        )
+    # Ambil selected_device dari command_data
+    command_parts = command_data.split('|')
+    selected_device = command_parts[2]
+
+    if selected_device == os.getenv('DEVICE_ID'):
+        await context.bot.send_message(chat_id=chat_id, text="Memulai proses restart bot...")
+
+        # Panggil run_bot.sh dengan argumen 'stop' lalu 'start'
+        try:
+            subprocess.run(['/www/assisten/bot/run_bot.sh', 'stop'], check=True)
+            await context.bot.send_message(chat_id=chat_id, text="Proses bot.py berhasil dihentikan. run_bot.sh akan memulai ulang bot secara otomatis.")
+            subprocess.Popen(['/www/assisten/bot/run_bot.sh', 'start'])
+        except FileNotFoundError:
+            await context.bot.send_message(chat_id=chat_id, text="❌ Gagal: File run_bot.sh tidak ditemukan.")
+        except subprocess.CalledProcessError as e:
+            await context.bot.send_message(chat_id=chat_id, text=f"❌ Gagal: Terjadi error saat menjalankan run_bot.sh: {e}")
+    else:
+        logger.info("Perintah /reload_bot diabaikan karena tidak cocok dengan DEVICE_ID.")
+        await context.bot.send_message(chat_id=chat_id, text="Perintah ini hanya dapat dijalankan di perangkat yang aktif.")
