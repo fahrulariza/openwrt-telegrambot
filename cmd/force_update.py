39ef4e8638083c5956d050e7e646f1653b8f82b4
fahrulariza <131193627+fahrulariza@users.noreply.github.com>
2025-08-17 00:57:22 +0700

    Update force_update.py

--- cmd/force_update.py
+++ cmd/force_update.py
@@ -5,7 +5,7 @@ from telegram.ext import ContextTypes
 
 logger = logging.getLogger(__name__)
 
-async def execute(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def execute(update: Update, context: ContextTypes.DEFAULT_TYPE, command_data: str) -> None:
     """Menangani perintah /force_update untuk memaksa pembaruan di semua perangkat."""
     
     chat_id = update.effective_chat.id
